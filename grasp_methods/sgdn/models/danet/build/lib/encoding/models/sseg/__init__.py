79f7f1784aabbf1557ddc4c199023ece4936e212
Dexin Wang <17853142856@163.com>
2023-06-25 22:10:29 +0800

    Add files via upload

---
+++ grasp_methods/sgdn/models/danet/build/lib/encoding/models/sseg/__init__.py
@@ -0,0 +1,24 @@
+from .base import *
+from .fcn import *
+from .psp import *
+from .fcfpn import *
+from .atten import *
+from .encnet import *
+from .deeplab import *
+from .upernet import *
+from .dran import *
+from .danet import *
+
+def get_segmentation_model(name, **kwargs):
+    models = {
+        'fcn': get_fcn,
+        'psp': get_psp,
+        'fcfpn': get_fcfpn,
+        'atten': get_atten,
+        'encnet': get_encnet,
+        'upernet': get_upernet,
+        'deeplab': get_deeplab,
+        'dran':get_dran,
+        'danet': get_danet
+    }
+    return models[name.lower()](**kwargs)
